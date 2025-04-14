55464aadeff268b8ae854898576cc7b80e19eecb
Francisco Tevez <franciscoraultevez@gmail.com>
2025-04-14 19:22:47 -0300

    primera entrega con breve descripción de su funcionamiento. (aplicación de asignación de ingresos utilizando prompts y alerts)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # proyecto_js
+
+Aplicación de Asignación de Ingresos
+---
+El objeto del programa es asistir al usuario en la administración de sus ingresos. Se le solicitará dividir el 100% de sus ingresos en tantas partes como desee, para luego asignarles un nombre. Finalmente, deberá ingresar tanto su saldo actual como su saldo mensual, y con todo esto se le mostrará la distribución de sus ingresos.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 let incomeAllocations = [];
 
+const validateNumberPrompt = submittedPrompt => {
+    while (isNaN(submittedPrompt)){
+        submittedPrompt = prompt('No puede ingresar otra cosa que no sea un número.')
+    };
+    return submittedPrompt;
+};
+
 const requestIncomeAllocationsPercentages = () => {
     let remainingPercentage = 1;
     let percentageInput;
@@ -7,7 +14,7 @@ const requestIncomeAllocationsPercentages = () => {
 
     while (remainingPercentage > 0) {
         percentageInput = parseInt(prompt(
-            'Por favor, ingrese un porcentaje (0-100) para dedicar a una categoría de gastos:')) / 100;
+            'Por favor, ingrese un porcentaje (0-100) que desearía dedicar de sus ingresos a una categoría de gastos:')) / 100;
         while (
             percentageInput > 1
             || (remainingPercentage - percentageInput) < 0
@@ -24,7 +31,13 @@ const requestIncomeAllocationsPercentages = () => {
         alert(`Porcentaje Restante: ${remainingPercentage * 100}%`)
     };
 
-    incomeAllocations.forEach(allocation => incomeAllocationsPercentages.push((allocation.percentage * 100).toString() + '%'));
+    // incomeAllocations.forEach(allocation => incomeAllocationsPercentages.push((allocation.percentage * 100).toString() + '%'));
+
+    // Para cumplir con el uso de lo visto en clase, hice lo mismo que hubiera hecho con forEach con un for común.
+    for (let i = 0; i < incomeAllocations.length; i++) {
+        const allocation = incomeAllocations[i];
+        incomeAllocationsPercentages.push((allocation.percentage * 100).toString() + '%')
+    }
     alert(`100% alcanzado. Sus gastos se dividirán en: ${incomeAllocationsPercentages.join(' + ')}`)
 };
 
@@ -33,20 +46,20 @@ const requestIncomeAllocationsNames = () => {
     incomeDistribution = [];
     incomeAllocations.forEach(allocation => {
         allocation.category = prompt(`¿A qué le dedicará el ${allocation.percentage * 100}% de su ingreso?`);
-        while (allocation.category == '') {
+        while (allocation.category == '' || allocation.category == null) {
             allocation.category = prompt(`Es necesario que asigne categorías a sus gastos, ¿a qué le dedicará el ${allocation.percentage * 100}% de su ingreso?`);
         };
         incomeDistribution.push(`${allocation.category}: ${allocation.percentage * 100}%`);
     });
-    alert(`¡Muy bien! Esta es tu distribución de ingresos: \n${incomeDistribution.join('\n')}`);
+    alert(`¡Muy bien! Esta es su distribución de ingresos: \n${incomeDistribution.join('\n')}`);
 };
 
 const requestIncome = () => {
-    let balance = prompt('Ahora por favor ingrese su balance (dinero total) actual:');
-    let monthlyIncome = prompt('Ingrese su saldo mensual:');
+    let balance = validateNumberPrompt(prompt('Ahora por favor ingrese su balance (dinero total) actual:'));
+    let monthlyIncome = validateNumberPrompt(prompt('Ingrese su saldo mensual:'));
     let appliedIncomeDistribution = [];
-    incomeAllocations.forEach(allocation => appliedIncomeDistribution.push(`${allocation.category}: $${allocation.percentage * balance} disponible + $${allocation.percentage * monthlyIncome} mensual`));
-    alert(`Esta es la distribución de tu dinero:\n${appliedIncomeDistribution.join('\n')}`)
+    incomeAllocations.forEach(allocation => appliedIncomeDistribution.push(`${allocation.category} (${allocation.percentage*100}%): $${allocation.percentage * balance} disponible + $${allocation.percentage * monthlyIncome} mensual`));
+    alert(`Esta es la distribución de su dinero:\n${appliedIncomeDistribution.join('\n')}`)
 };
 
 requestIncomeAllocationsPercentages();
